e49f3d0acf4f62820692f4925eaa7babed762010
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-02-25 23:00:05 -0300

    Player added, final adjustments

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function Maze(rows, columns){
     for (let key of directionKeys) {
       let dx = directions[key][0];
       let dy = directions[key][1];
-      if (isValidPos(x + dx, y + dy, maze.board) === false ||
+      if (isValidPosition(x + dx, y + dy, maze.board) === false ||
         maze.board[x + dx][y + dy].visited === true)
       {
         continue;
@@ -94,25 +94,45 @@ function Maze(rows, columns){
   }
 }
 
-function drawMaze(bitmap) {
-  let size = Math.floor(canvas.width / bitmap.length);
-  for (let i = 0; i < bitmap.length; ++i) {
-    for (let j = 0; j < bitmap[i].length; ++j) {
-      switch (bitmap[i][j]) {
+function drawMaze(map) {
+  let size = Math.floor(canvas.width / map.length);
+  for (let i = 0; i < map.length; ++i) {
+    for (let j = 0; j < map[i].length; ++j) {
+      switch (map[i][j]) {
         case side['wall']:
           ctx.fillStyle = 'black';
           break;
         case side['empty']:
           ctx.fillStyle = 'white';
           break;
-        case side['exit']:
-          ctx.fillStyle = '#26dd22';
+        case side['stairs']:
+          ctx.fillStyle = '#red';
           break;
       }
       ctx.fillRect(i*size, j*size, size, size);
     }
   }
 }
+
+//Shuffle 
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; --i) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+function isValidPosition(x, y, plane) {
+  return (x >= 0 && x < plane.length && y >= 0 && y < plane[x].length);
+}
+
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
 
 function keyDownHandler(event) {
   switch (event.keyCode) {
@@ -131,6 +151,88 @@ function keyDownHandler(event) {
   }
 }
 
-window.onload = () => {
-  test1(0);
-};
+function keyUpHandler() {
+  switch (event.keyCode) {
+    case 31:
+      rightPressed = false;
+      break;
+    case 37:
+      leftPressed = false;
+      break;
+    case 40:
+      downPressed = false;
+      break;
+    case 38:
+      upPressed = false;
+      break;
+  }
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function Player(map) {
+  this.pos = {x: 1, y: 1};
+  this.color = 'green';
+
+  let size = Math.floor(canvas.width / map.length);
+
+  this.draw = function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.pos.x*size, this.pos.y*size, size, size);
+  }
+
+  this.clear = function() {
+    ctx.clearRect(this.pos.x*size, this.pos.y*size, size, size);
+  }
+
+  this.move = function(dir) {
+    let dx = this.pos.x + directions[dir][0];
+    let dy = this.pos.y + directions[dir][1];
+    if (isValidPosition(dx, dy, map) && map[dx][dy] !== side['wall']) {
+      this.clear();
+      this.pos.x = dx;
+      this.pos.y = dy;
+      this.draw();
+    }
+  }
+}
+
+const maze = new Maze(32, 32);  //Change map size
+const map = maze.toMap();
+const player = new Player(map);
+
+//FPS = Frames per Second and FMT = Frame Minimum Time
+const FPS = 10;
+const FMT = (1000/60) * (60/FPS) - (1000/60) * 0.5;
+var lastFrameTime = 0;
+
+drawMaze(map);
+player.draw();
+
+function main(time) {
+  if (time - lastFrameTime < FMT) {
+    requestAnimationFrame(main);
+    return;
+  }
+
+  if (rightPressed){
+    player.move('E')
+  } else if (leftPressed){
+    player.move('W');
+  } else if (upPressed){
+    player.move('S');
+  } else if (downPressed){
+    player.move('N');
+  }
+
+  if (map[player.pos.x][player.pos.y] === side['exit']) {
+    window.alert('You found the exit');
+    return;
+  }
+
+  lastFrameTime = time;
+  requestAnimationFrame(main);
+}
+
+requestAnimationFrame(main);
